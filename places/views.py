fa03234a0f7f14a17c8af27cc874d2d5824ab49b
Natalia <nata.mamseva.93@bk.ru>
2022-04-06 00:37:40 +0600

    add places model, views, urls

--- places/views.py
+++ places/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Place
 
 # Create your views here.
+
+def places(request):
+    place_objects = Place.objects.all()
+    return render(request, 'places.html', {'places': place_objects})
+
+def page_not_found_view(request, exception):
+    return render(request, '404.html', status=404)
--- tourpage/settings.py
+++ tourpage/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m1a-n&l)sz&u)9xazx%v1ztzs^+i641yzm@3s4u!c%^o0n%5*2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'tourpage.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
